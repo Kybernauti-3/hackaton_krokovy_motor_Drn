aee73477ec102b2ac922ddd2b8bd56f5db63821a
D32\zak <spseplze>
2022-10-17 13:58:54 +0200

    vrneni_moturku

---
+++ main.py
@@ -0,0 +1,26 @@
+import utime as time
+import machine
+
+piny = [
+    machine.Pin(1, machine.Pin.OUT),
+    machine.Pin(2, machine.Pin.OUT),
+    machine.Pin(4, machine.Pin.OUT),
+    machine.Pin(5, machine.Pin.OUT),
+]
+
+otocka = [
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1]
+    ]
+
+while True:
+    for krucek in otocka:
+        for i in range(len(piny)):
+            piny[i].value(krucek[i])
+            time.sleep(0.1)
+            
+
+
+
